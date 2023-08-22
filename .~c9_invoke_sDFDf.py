d5eb2f0a644a4a71d2c28caad1dabd6989cbfc45
brianarorie <99783729+brianarorie@users.noreply.github.com>
2023-08-22 13:48:00 -0400

    Delete .~c9_invoke_sDFDf.py

--- .~c9_invoke_sDFDf.py
+++
@@ -1,8 +0,0 @@
-def ec2_generated_name(letters , numbers)
-x = input('How many instance names do you want ?')
-    if x in ec2_generate>= 1:
-       print('')
-       
-        print('')
-
-ec2_generated_name('a-z' , '0-9')
