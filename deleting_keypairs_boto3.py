97c6d17453a921064aaec9fa530d2c44f5fa7e39
brianarorie <99783729+brianarorie@users.noreply.github.com>
2023-09-05 21:45:16 -0400

    Update deleting_keypairs_boto3.py

--- deleting_keypairs_boto3.py
+++ deleting_keypairs_boto3.py
@@ -4,7 +4,7 @@ import boto3
 # Create an EC2 client instance using boto3
 ec2 = boto3.client('ec2')
 
-# Delete a key pair using the specified key name.
+# Delete a key pair using the specified key name
 response = ec2.delete_key_pair(
     KeyName='boto3 keys',  # Name of the key pair to delete
 )
